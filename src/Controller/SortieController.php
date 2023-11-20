043afa5ebfe01193080245e3da1f0a6cca91af5c
Greggory Picoli <greggorypicoli@gmail.com>
2023-11-20 16:27:45 +0100

    Affichage de Actions sur tableux accuiel. Et de details de sortie sur page details de sortie.

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Repository\LieuRepository;
+use App\Repository\ParticipantRepository;
+use App\Repository\SortieRepository;
+use App\Repository\VilleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/sortie', name: 'app_sortie')]
+class SortieController extends AbstractController
+{
+    #[Route('/details/{id<\d+>}', name: '_details')]
+    public function sortie(int $id, ParticipantRepository $participantRepository, SortieRepository $sortieRepository, Security $security): Response
+    {
+        $sortie = $sortieRepository->find($id);
+        $participantSortie = $participantRepository->findAll();
+            return $this->render('sortie/details.html.twig', [
+                "sortie" => $sortie,
+                "participant" =>$participantSortie
+            ]);
+    }
+
+    #[Route('/create', name: '_create')]
+    public function create(LieuRepository $lieuRepository, VilleRepository $villeRepository, SortieRepository $sortieRepository, Security $security): Response
+    {
+        $ville=$villeRepository->findAll();
+        $lieu=$lieuRepository->findAll();
+        return $this->render('sortie/create.html.twig', [
+            "ville" => $ville,
+            "lieu" => $lieu
+        ]);
+    }
+
+}
