4a04bb5ea3dc9676e9deaa855fa557f2057cbcc4
Foulcalamar <maeva.westermeyer@gmail.com>
2023-11-23 07:57:39 +0100

    Tentaitve de creation du formulaire de creation

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 
+use App\Entity\Sortie;
+use App\Form\SortieType;
 use App\Repository\LieuRepository;
 use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
@@ -29,12 +31,14 @@ class SortieController extends AbstractController
     #[Route('/create', name: '_create')]
     public function create(LieuRepository $lieuRepository, VilleRepository $villeRepository, SortieRepository $sortieRepository, Security $security): Response
     {
+        $nomSortie = new Sortie();
+
         $ville=$villeRepository->findAll();
         $lieu=$lieuRepository->findAll();
         return $this->render('sortie/create.html.twig', [
             "ville" => $ville,
-            "lieu" => $lieu
+            "lieu" => $lieu,
+
         ]);
     }
-
 }
