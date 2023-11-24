93ea82ccec64f53b50c8d63cc3ab8e05990cae7c
Aleksei Pavlyshin-Gadais <al_pavlyshin-gadais@protonmail.com>
2023-11-24 14:20:20 +0100

    Correction d'un bug de fusion

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,6 +12,7 @@ use App\Repository\SortieRepository;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,7 +20,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(SortieRepository $sortieRepository, EntityManagerInterface $entityManager, EtatRepository $etatRepository, Security $security): Response {
+    public function index(Request $request, SortieRepository $sortieRepository, EntityManagerInterface $entityManager,
+                          EtatRepository $etatRepository, Security $security): Response {
 
         $currentTime = new DateTime();
 
